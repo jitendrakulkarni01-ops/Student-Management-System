206147777334d8e8047e51f301a4208d6ad07a59
Jitendra Kulkarni <jitendrakulkarni01@gmail.com>
2026-06-20 15:58:52 +0530

    Add requirement file and gitignore file

--- student_management/settings.py
+++ student_management/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'student_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'student_management_db',
+        'USER': 'postgres',
+        'PASSWORD': 'user@1318',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
